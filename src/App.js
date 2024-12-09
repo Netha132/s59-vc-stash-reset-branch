9b3351684c4e08dc68470473192c5b2853fd6d23
VivekNetha <90238482+Netha132@users.noreply.github.com>
2024-12-09 14:22:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+        <h1>Version Controller using Git</h1>
+        <h2>Creater:Linus Torvalds</h2>
+        <img src='./gitImg/gitImg.jpg' style={{width:"200px"}}></img>
+
+        <h1>SVN version control</h1>
+        <img src='./svnimg/svnImg.jpg'></img>
+
+
+        <h1>Microsoft Visual SourceSafe</h1>
+        <img src='./vssImg/vssimg.jpg'></img>
+
+    </div>
+  );
+}
+
+export default App;
